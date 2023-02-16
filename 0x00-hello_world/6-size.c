e5d7f3998c8ad21ef5cf7bb8f9233805c7c83f22
yared-t <yaredtesfaye156@gmail.com>
2023-02-16 13:54:30 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- * *main - Entry point
- * 
- * return: alawys (0) (success)
+ * main  - Used to display the bytes of data types
+ * return: alawys 0 (Success)
  */
 int main(void)
-                {
-printf("size of a char : %d byte(s)\n" ,sizeof(char));
-printf("size of an int : %d byte(s)\n" ,sizeof(int));
-printf("size of a long int : %d byte(s)\n" ,sizeof(long int));
-printf("size of a long long int : %d byte(s)\n" ,sizeof(long long int));
-printf("size of a float : %d byte(s)\n" ,sizeof(float));
+{
+printf("size of a char : %d byte(s)\n", sizeof(char));
+printf("size of an int : %d byte(s)\n", sizeof(int));
+printf("size of a long int : %d byte(s)\n", sizeof(long int));
+printf("size of a long long int : %d byte(s)\n", sizeof(long long int));
+printf("size of a float : %d byte(s)\n", sizeof(float));
 return (0);
-                }
+}
