e76edcc4c98ac44bc5f2f280aa1709b80f369472
yared-t <yaredtesfaye156@gmail.com>
2023-02-16 11:37:03 -0800

    print using outs

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stduio>
+/**
+ * *main - Entry point
+ * 
+ * return: alawys (0) (success)
+ */
+int main(void)
+		{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+		}
