0c7c6137297a233a35eb3241b5f188218881c994
yared-t <yaredtesfaye156@gmail.com>
2023-02-16 14:02:45 -0800

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-# include < stdio.h>
+# include <stdio.h>
 # include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	Return(1);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return(1);
 }
