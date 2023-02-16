e56733d2aab535e41b99736cc58ade79c9d4e58f
yared-t <yaredtesfaye156@gmail.com>
2023-02-16 14:11:34 -0800

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 # include <stdio.h>
 # include <unistd.h>
 /**
- * main - A c program that prints exactly the given data and a line to the standard error
- * Return 1 (Success)
+ * main - A c program that prints exactly the given data and the standard error
+ * Return: always 1 (Success)
  */
 int main(void)
 {
