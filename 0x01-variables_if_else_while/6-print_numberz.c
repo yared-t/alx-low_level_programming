34cad6104897e09a5fbfbda2824cbe381c1b5b77
yared-t <yaredtesfaye156@gmail.com>
2023-02-16 23:40:46 -0800

    6 print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Aprigram used to write a number
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n = 48;
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0) ;
+}
