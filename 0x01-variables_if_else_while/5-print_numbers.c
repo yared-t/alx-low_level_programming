1162bf413a70d94429e5bb161ad6294b2d31f66c
yared-t <yaredtesfaye156@gmail.com>
2023-02-16 23:28:01 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -A program used to write from 0 to 10
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+	for (n = 0;n < 10;n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
