df88a74b70009f0a1a6a2be8e3c7fac1bbd79a66
yared-t <yaredtesfaye156@gmail.com>
2023-02-21 00:15:37 -0800

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - prints whether alpha or not
+ * Return: 1 or Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
