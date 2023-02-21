dff9ac7cf931df7d376e86dda83206db8ac23e32
yared-t <yaredtesfaye156@gmail.com>
2023-02-21 03:20:47 -0800

    print hte last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,6 +2,7 @@
 
 /**
  * print_last_digit -used to print the last digit
+ * @n: used to store a number
  * Return: the last digit
  */
 int print_last_digit(int n)
@@ -12,7 +13,7 @@ int print_last_digit(int n)
 		n = -n;
 	a = n % 10;
 	if (a < 0)
-                a = -a;
-        _putchar(a + '0');
+		a = -a;
+	_putchar(a + '0');
 	return (a);
 }
