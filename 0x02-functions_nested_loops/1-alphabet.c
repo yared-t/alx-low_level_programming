064b9f8e5a9e2b49b1ceb2d5ed225fd059acf5e6
yared-t <yaredtesfaye156@gmail.com>
2023-02-20 23:50:46 -0800

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print _alphabate in lower case
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <+ 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
