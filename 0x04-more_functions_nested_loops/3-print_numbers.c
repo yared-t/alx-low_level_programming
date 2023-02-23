b98c73ff3031b6b010181f1cf87b29203522add1
yared-t <yaredtesfaye156@gmail.com>
2023-02-23 06:08:17 -0800

    more functions and nested loop

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,15 @@
 
 /**
  * print_numbers - program used to print 0-9
- * @n: used to store a number
  */
 void print_numbers(void)
 {
 	int n;
 
 	for (n = 48; n < 58; n++)
+	{
 		_putchar(n);
+	}
+
 	_putchar('\n');
 }
