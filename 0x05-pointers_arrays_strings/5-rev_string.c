1d9885fb34238311c8199cc5a6bf331a557970c3
yared-t <yaredtesfaye156@gmail.com>
2023-02-27 00:01:23 -0800

    pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - used to reverse the string
+ * @s: used to store char
+ */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int counter = 0;
+	int i;
+
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
+}
