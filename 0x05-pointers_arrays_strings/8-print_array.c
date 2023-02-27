bbc95c53dc3864754ac3a0d7afd8c57d8a6d1126
yared-t <yaredtesfaye156@gmail.com>
2023-02-27 00:27:17 -0800

    pointers arays and strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - print arrays
+ * @n: used to sstore number
+ * @a: used to store address
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+			printf("\n");
+}
