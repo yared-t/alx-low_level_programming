fff8fbb75ea5e29f87e1c88b158eda5e0fe53c11
yared-t <yaredtesfaye156@gmail.com>
2023-03-07 22:38:27 -0800

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - used to count the string length
+ * @s: used to store a string
+ * Return: 0 (Success)
+ */
+int _strlen_recursion(char *s)
+{
+	int n;
+
+	n = 0;
+	if (*s)
+	{
+		n++;
+		n += _strlen_recursion(s+1);
+	}
+	return (n);
+}
