264583fe9e16e7e8b136d7faeabeb19bccef3984
yared-t <yaredtesfaye156@gmail.com>
2023-03-13 00:30:56 -0700

    args

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-
 #include "main.h"
-
-
-
 /**
  * main - prints the name of the program
  * @argc: number of arguments
@@ -13,8 +9,5 @@
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
-
-
-
 	return (0);
 }
