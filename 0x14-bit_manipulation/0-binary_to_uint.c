da13d3a7c33580325adb5929408648be225b17cc
yared-t <yaredtesfaye156@gmail.com>
2023-04-29 12:37:20 -0700

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,19 +7,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int len = 0;
 	int i = 0;
 
-	while (b[len])
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
 	{
-		len++;
-	}
-	for (i = 0; i < len; i++)
-	{
-		if (b[len - i - 1] == '1')
-			num += 1 << i;
-		else if (b[len - i - 1] != '0')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		num 2 * num + (b[i] - '\0');
 	}
 	return (num);
 }
